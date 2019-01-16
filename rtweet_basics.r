210ebab7a25f86c335ab4ac30101fb6a2d9f866d
EFastner <eric.fastner@gmail.com>
2019-01-16 14:36:35 -0600

    Initial commit of some basic rtweet functions and data exploration

---
+++ rtweet_basics.r
@@ -0,0 +1,26 @@
+#Search all tweets
+rt <- search_tweets(
+  q = "#rstats",
+  n = 25,
+  include_rts = FALSE
+)
+
+#Search a timeline
+tl <- get_timeline(
+  user = "@Lin_Manuel",
+  n = 5000
+)
+
+text_only <- tl$text
+
+gmorning <- dplyr::filter(tl, grepl("Gmorning", tl$text))
+gnight <- dplyr::filter(tl, grepl("Gnight", tl$text))
+
+gmorning$date <- as.Date(gmorning$created_at)
+gnight$date <- as.Date(gnight$created_at)
+
+gmorning_dates <- 
+  gmorning %>%
+  group_by(date) %>%
+  summarise(number_of_tweets = n()) %>%
+  arrange(desc(number_of_tweets))
